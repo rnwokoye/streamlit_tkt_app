7c3342ec57f34335d185afbabc922b28978541f2
Richard Nwokoye <RNwokoye@gmail.com>
2024-01-29 12:54:59 -0500

    usig yaml for auth

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ st.title("Traffic Ticket Assignement :male-pilot: 	:vertical_traffic_light:")
 # :camera:
 
 log_in()
-username = st.session_state.username
-name = st.session_state.name
+# username = st.session_state.username
+# name = st.session_state.name
 
-st.write(st.session_state)
+# st.write(st.session_state)
 # if username is not None and name is not None:
 #     if is_admin(name):
 #         admin_page.display_data()
--- login.py
+++ login.py
@@ -1,40 +1,73 @@
 import streamlit as st
 import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+
+
+# def log_in():
+#     # Make a deep copy of the authentication config to avoid modifying st.secrets
+#     authentication_config = dict(st.secrets["credentials"])
+#     usernames = authentication_config["usernames"]
+#     # Convert the usernames to lowercase to match the expectation of the stauth.Authenticate class
+#     authentication_config["usernames"] = {
+#         key.lower(): value for key, value in usernames.items()
+#     }
+
+#     authenticator = stauth.Authenticate(
+#         authentication_config,
+#         st.secrets["cookie"]["name"],
+#         st.secrets["cookie"]["key"],
+#         st.secrets["cookie"]["expiry_days"],
+#         st.secrets["preauthorized"],
+#     )
+
+#     authenticator.login("Login", "main")
+#     name = st.session_state.name
+#     username = st.session_state.username
+
+#     if st.session_state["authentication_status"]:
+#         st.write(f'Welcome *{st.session_state["name"]}*')
+#         authenticator.logout("Logout", "sidebar", key="unique_key")
+#         if st.button("Reset"):
+#             authenticator.reset_password(st.session_state["username"], "Reset_password")
+#         return username, name
+#     elif st.session_state["authentication_status"] is False:
+#         st.error("Username/password is incorrect")
+#         if st.button("Forgot password"):
+#             authenticator.forgot_password("Forgot_Passsword")
+#     elif st.session_state["authentication_status"] is None:
+#         st.warning("Please enter your username and password")
+#         if st.button("register"):
+#             authenticator.register_user("Register From")
+#             st.write("Done")
+
+
+# authentication_config = st.secrets["credentials"]
 
 
 def log_in():
-    # Make a deep copy of the authentication config to avoid modifying st.secrets
-    authentication_config = dict(st.secrets["credentials"])
-    usernames = authentication_config["usernames"]
-    # Convert the usernames to lowercase to match the expectation of the stauth.Authenticate class
-    authentication_config["usernames"] = {
-        key.lower(): value for key, value in usernames.items()
-    }
+    with open("config.yaml") as file:
+        config = yaml.load(file, Loader=SafeLoader)
 
     authenticator = stauth.Authenticate(
-        authentication_config,
-        st.secrets["cookie"]["name"],
-        st.secrets["cookie"]["key"],
-        st.secrets["cookie"]["expiry_days"],
-        st.secrets["preauthorized"],
+        config["credentials"],
+        config["cookie"]["name"],
+        config["cookie"]["key"],
+        config["cookie"]["expiry_days"],
+        config["preauthorized"],
     )
 
-    authenticator.login("Login", "main")
-    name = st.session_state.name
-    username = st.session_state.username
+    authenticator.login()
 
     if st.session_state["authentication_status"]:
+        authenticator.logout()
         st.write(f'Welcome *{st.session_state["name"]}*')
-        authenticator.logout("Logout", "sidebar", key="unique_key")
-        if st.button("Reset"):
-            authenticator.reset_password(st.session_state["username"], "Reset_password")
-        return username, name
+        st.title("Some content")
     elif st.session_state["authentication_status"] is False:
         st.error("Username/password is incorrect")
-        if st.button("Forgot password"):
-            authenticator.forgot_password("Forgot_Passsword")
     elif st.session_state["authentication_status"] is None:
         st.warning("Please enter your username and password")
-        if st.button("register"):
-            authenticator.register_user("Register From")
-            st.write("Done")
+
+
+if __name__ == "__main__":
+    log_in()
