797472fe6bdce3ba569e82e5b3fa68dbc7072dca
Richard Nwokoye <RNwokoye@gmail.com>
2024-01-29 15:22:16 -0500

    debug

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ st.title("Traffic Ticket Assignement :male-pilot: 	:vertical_traffic_light:")
 
 log_in()
 name = st.session_state.name
+st.write(name)
 if name is not None:
     if is_admin(name):
         admin_page.display_data()
