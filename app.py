6f7c0c0188584a9193555084b21d77d8056425e4
Richard Nwokoye <RNwokoye@gmail.com>
2024-01-28 19:01:27 -0500

    debugging

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 # from create_tkt import *
 from login import log_in
-from create_tkt import *
+
+# from create_tkt import *
 
 
 st.title("Traffic Ticket Assignement :male-pilot: 	:vertical_traffic_light:")
@@ -11,8 +12,10 @@ st.title("Traffic Ticket Assignement :male-pilot: 	:vertical_traffic_light:")
 log_in()
 username = st.session_state.username
 name = st.session_state.name
-if username is not None and name is not None:
-    if is_admin(name):
-        admin_page.display_data()
-    else:
-        insert_offense(run_program(username))
+
+st.write(st.session_state)
+# if username is not None and name is not None:
+#     if is_admin(name):
+#         admin_page.display_data()
+#     else:
+#         insert_offense(run_program(username))
